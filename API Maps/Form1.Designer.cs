69217f88577bd6dbaba6fcc5770cd4607128e5cd
AlexPinekone <127763929+AlexPinekone@users.noreply.github.com>
2024-09-11 19:30:39 -0600

    Add files via upload

---
+++ API Maps/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace API_Maps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.userControl11 = new API_Maps.UserControl1();
+            this.btnCalcularDistancia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(0, 0);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(800, 407);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.userControl11;
+            // 
+            // btnCalcularDistancia
+            // 
+            this.btnCalcularDistancia.Location = new System.Drawing.Point(351, 413);
+            this.btnCalcularDistancia.Name = "btnCalcularDistancia";
+            this.btnCalcularDistancia.Size = new System.Drawing.Size(96, 34);
+            this.btnCalcularDistancia.TabIndex = 1;
+            this.btnCalcularDistancia.Text = "Calcula";
+            this.btnCalcularDistancia.UseVisualStyleBackColor = true;
+            this.btnCalcularDistancia.Click += new System.EventHandler(this.btnCalcularDistancia_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcularDistancia);
+            this.Controls.Add(this.elementHost1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private UserControl1 userControl11;
+        private System.Windows.Forms.Button btnCalcularDistancia;
+    }
+}
+
---
+++ API Maps/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System.Windows.Controls;
+using System.Threading;
+
+namespace API_Maps
+{
+    public partial class Form1 : Form
+    {
+        private const string llaveApi = "API";
+        public Form1()
+        {
+            InitializeComponent();
+
+            Thread coor = new Thread(cicloDatos);
+            coor.Start();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        bool ya=false;
+
+        double latitud;
+        double longitud;
+        bool state;
+        bool cicloF=true;
+        private void cicloDatos()
+        {
+            
+            while (cicloF)
+            {
+
+                state = this.userControl11.getState();
+                if (ya == false && state== true)
+                {
+                    ya = true;
+                    latitud = this.userControl11.latitude;
+                    longitud = this.userControl11.longitude;
+                    MessageBox.Show("Coordenadas: "+latitud+" , "+longitud);
+                    while (state == true) ;
+                }
+                   
+            }
+
+        }
+
+
+        private async Task<double> CalcularDistanciaAsync(double latitud1, double longitud1, double latitud2, double longitud2)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string requestUri = $"https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins={latitud1},{longitud1}&destinations={latitud2},{longitud2}&travelMode=driving&key={llaveApi}";
+
+                HttpResponseMessage response = await client.GetAsync(requestUri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var result = JsonSerializer.Deserialize<DistanceMatrixResult>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                    return result?.ResourceSets[0]?.Resources[0]?.Results[0]?.TravelDistance ?? 0;
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Error al calcular la distancia: {response.StatusCode}");
+                }
+            }
+        }
+
+        private async void btnCalcularDistancia_Click_1(object sender, EventArgs e)
+        {
+                                                        //22.151844361388388, -100.97116408283527
+            double lati = latitud; //48.8588446; // 22.145123652960574, -101.01551373119145
+            double longi = longitud;//2.2943506; // 22.12705240350552, -101.00200708964489
+            double tangamangaLat = 22.12705240350552;
+            double tangamangaLon = -101.00200708964489;
+
+            double distancia = await CalcularDistanciaAsync(lati, longi, tangamangaLat, tangamangaLon);
+
+            ya = false;
+            state = false;
+            this.userControl11.state = false;
+
+            MessageBox.Show($"La distancia entre la posicion dada y el parque morales es de aproximadamente {distancia/2} kilómetros.");
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cicloF = false;
+        }
+    }
+
+    public class DistanceMatrixResult
+    {
+        public ResourceSet[] ResourceSets { get; set; }
+    }
+
+    public class ResourceSet
+    {
+        public Resource[] Resources { get; set; }
+    }
+
+    public class Resource
+    {
+        public Result[] Results { get; set; }
+    }
+
+    public class Result
+    {
+        public double TravelDistance { get; set; }
+    }
+
+    
+
+}
